4d7d8ee8758ba44e1f616702610392f5ce7896d7
krishnan9787 <k65494989@gmail.com>
2026-03-10 23:49:17 -0700

    feature/UC2-display-name

---
+++ HelloApp_UC2.java
@@ -0,0 +1,9 @@
+public class HelloApp_UC2 {
+    public static void main(String[] args){
+        String name = args[0];
+        System.out.println("Hello , " + name + "!");
+
+        String name1 = args[1];
+        System.out.println("Hello , " + name1 + "!");
+    }
+}
